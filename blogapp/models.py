836db53f194844f7a3bd7483104891a708d5ef2d
newmagicz <n.chanatip@hotmail.com>
2022-01-10 21:00:11 +0700

    update style

--- blogapp/models.py
+++ blogapp/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    
 
     def __str__(self):
         return self.title
